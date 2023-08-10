faca13a165575737520052a0692c6da7cdf7e372
enoch <enokkang@gmail.com>
2023-08-10 13:17:22 -0500

    adds description imgurl hardcode

--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -4,13 +4,21 @@ export function ProductsNew() {
       <h1>Products New</h1>
       <form action="#" method="POST">
         <label for="html">Name: </label>
-
         <input type="text" name="name" />
         <br></br>
-        <label for="html">Price: </label>
 
+        <label for="html">Price: </label>
         <input type="text" name="price" />
         <br></br>
+
+        <label for="html">Description: </label>
+        <textarea id="description" name="description" />
+        <br></br>
+
+        <label for="html">Image URL: </label>
+        <input type="text" name="image_url" />
+        <br></br>
+
         <button type="submit">Submit</button>
       </form>
     </div>
