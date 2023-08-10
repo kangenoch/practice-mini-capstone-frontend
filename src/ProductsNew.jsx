24640db3bca6b6ea1cc0ae0b2edd552e59e355dd
enoch <enokkang@gmail.com>
2023-08-10 13:31:06 -0500

    adds htmlfor=html

--- src/ProductsNew.jsx
+++ src/ProductsNew.jsx
@@ -3,19 +3,19 @@ export function ProductsNew() {
     <div>
       <h1>Products New</h1>
       <form action="#" method="POST">
-        <label for="html">Name: </label>
+        <label htmlFor="html">Name: </label>
         <input type="text" name="name" />
         <br></br>
 
-        <label for="html">Price: </label>
+        <label htmlFor="html">Price: </label>
         <input type="text" name="price" />
         <br></br>
 
-        <label for="html">Description: </label>
+        <label htmlFor="html">Description: </label>
         <textarea id="description" name="description" />
         <br></br>
 
-        <label for="html">Image URL: </label>
+        <label htmlFor="html">Image URL: </label>
         <input type="text" name="image_url" />
         <br></br>
 
